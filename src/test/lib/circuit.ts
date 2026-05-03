058c7c684a3302130515676c3c15c1dddc1036d9
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2026-05-03 21:51:20 +0200

    initial commit

--- src/test/lib/circuit.ts
+++ src/test/lib/circuit.ts
@@ -1,10 +1,12 @@
 // Wrap circom_tester so each test file picks the wasm + include path the same way.
 
 import * as path from "path";
+import { fileURLToPath } from "url";
 // circom_tester ships without TS types
 // @ts-ignore
 import { wasm as wasmTester } from "circom_tester";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const SRC_DIR = path.join(__dirname, "..", "..");
 const NODE_MODULES = path.join(SRC_DIR, "..", "node_modules");
 
